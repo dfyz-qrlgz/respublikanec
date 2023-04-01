57d678d2323c6b2eb194bee12a70bbdc697f5d1e
DFYZ <ivan6179217@mail.ru>
2023-04-01 13:09:45 +0300

    ba bu beebu

---
+++ RAKTIKA_1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RAKTIKA_1.libraryDataSetTableAdapters;
+
+namespace RAKTIKA_1
+{
+    public partial class MainWindow : Window
+    {
+        authorsTableAdapter author = new authorsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            datagrid.ItemsSource = author.GetData();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            second window = new second();
+            window.ShowDialog();
+        }
+    }
+}
---
+++ RAKTIKA_1/second.xaml.cs
@@ -0,0 +1,32 @@
+﻿using RAKTIKA_1.libraryDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RAKTIKA_1
+{
+    public partial class second : Window
+    {
+        booksTableAdapter books = new booksTableAdapter();
+        public second()
+        {
+            InitializeComponent();
+            datagrid2.ItemsSource = books.GetData();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
